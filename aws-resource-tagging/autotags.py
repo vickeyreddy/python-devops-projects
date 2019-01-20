e0a3597f50bcbd962a42cfeabc125be73f8811b8
Vijay Gosai <vgosai@redhat.com>
2019-01-20 20:51:06 +0530

    Added features to autoupdate EC2 tags

--- aws-resource-tagging/autotags.py
+++ aws-resource-tagging/autotags.py
@@ -1,7 +1,8 @@
+#!/usr/bin/env python3
+
 import boto3
 import logging
 import threading
-import argparse
 from botocore.exceptions import ClientError
 
 FORMAT = "[%(levelname)-s %(asctime)-s %(threadName)-s ] %(message)s"
@@ -13,14 +14,22 @@ class AutoTagAWSResources(object):
     instances = {}
 
     def __init__(self, key, secret):
-        '''
-        :AWS Key key:
-        :AWS Secret Key secret:
-        '''
+        """
+        AWS Config Keys & Secrets
+        :AWS KEY: key
+        :AWS SECRET: secret
+        """
         self.key = key
         self.secret = secret
 
     def get_connection(self, conn_type='client', region='us-east-1', resource='ec2'):
+        """
+        Returns AWS Python Boto3 Connection
+        :params:conn_type: Boto3 Connection Type - client or resource - Default is client
+        :params: region: AWS Region Name - Default if us-east-1
+        :params: resource: Boto3 Resource Type - Default is ec2
+        :returns boto3 connection:
+        """
         logging.info("Starting a new connection for {} resource in {} region.".format(resource, region))
         try:
             if conn_type == 'resource':
@@ -29,11 +38,17 @@ class AutoTagAWSResources(object):
                 connection = boto3.client(resource, aws_access_key_id=self.key, aws_secret_access_key=self.secret, region_name=region)
         except ClientError as e:
             logging.error("Connection Error : {}".format(e))
+#        except Exception as e:
+#            logging.error("Exception : {}".format(e))
         else:
             logging.info("Successfully Connected using {} Connection!!".format(conn_type))
             return connection
 
     def get_regions(self):
+        """
+        Returns the list of all AWS REGIONS
+        :return: regions
+        """
         conn = self.get_connection()
         try:
             logging.info("Fetching AWS Regions.")
@@ -45,6 +60,11 @@ class AutoTagAWSResources(object):
             return regions
 
     def get_instances(self, region):
+        """
+        Returns a list of all the EC2 instances of given AWS Region
+        :param: region: AWS Region
+        :return: instances
+        """
         conn = self.get_connection(region=region)
         try:
             logging.info("Fetching AWS EC2 list for {} Region".format(region))
@@ -59,6 +79,10 @@ class AutoTagAWSResources(object):
             return self.instances
 
     def get_all_instances(self):
+        """
+        Returns a list of all the EC2 instances irrespective of the AWS Regions.
+        :return: instances
+        """
         threads = []
         regions = self.get_regions()
         for region in regions:
@@ -71,6 +95,12 @@ class AutoTagAWSResources(object):
         return self.instances
 
     def get_resource_owner(self, instance_id, region):
+        """
+        Returns the name of Owner who initially created the EC2 instance based on CloudTrail Events
+        :param instance_id: EC2 Instance ID
+        :param region: AWS Region Name
+        :return owner: Owner of the EC2 instance
+        """
         owner = None
         conn = self.get_connection(resource='cloudtrail', region=region)
         try:
@@ -86,26 +116,29 @@ class AutoTagAWSResources(object):
             return owner
 
     def update_ec2_tags(self, instance_id, region, **tags):
-        ec2tags = {}
+        """
+        Update EC2 Instance tags
+        :param instance_id: EC2 Instance ID
+        :param region: AWS Region Name
+        :param tags: Tags to set - "Owner=Vijay","OwnerEmail=vgosai@redhat.com","Environment=Production","CostCenter=711","Team=CP-DevOps"
+        :return:
+        """
         conn = self.get_connection(conn_type='resource', region=region)
         ec2info = conn.Instance(instance_id)
         logging.info("Starting EC2 Instance tagging for {} Instance in {} Region".format(instance_id, region))
         if ec2info.tags:
-            for i in range(0, len(ec2info.tags)):
-                ec2tags[ec2info.tags[i]['Key']] = ec2info.tags[i]['Value']
-
-            if "Owner" in ec2info.tags[i]['Key']:
-                if ec2info.tags[i]['Value'] is not None:
-                    logging.info("Tags are already up to dated for EC2 {} instance".format(instance_id))
-                    print("Tags are already up to dated for {} instance".format(instance_id))
-                else:
-                    for k, v in tags.items():
-                        conn.create_tags(Resources=[instance_id], Tags=[{'Key': k, 'Value': v}])
-                        logging.info("Successfully updated tags for EC2 {} instance".format(instance_id))
-                        print("Successfully added tags for EC2 {} instance".format(instance_id))
+            if list(filter(lambda x: x['Key'] == "Owner", ec2info.tags)):
+                logging.info("Owner tag is already present for EC {} instance, hence, no changes were made!!".format(instance_id))
+                print("Owner tag is already present for EC {} instance, hence, no changes were made!!".format(instance_id))
+            else:
+                for k, v in tags.items():
+                    conn.create_tags(Resources=[instance_id], Tags=[{'Key': k, 'Value': v}])
+                    logging.info("Successfully added tags for EC2 {} instance".format(instance_id))
+                print("Successfully added tags for EC2 {} instance".format(instance_id))
         else:
             for k, v in tags.items():
                 conn.create_tags(Resources=[instance_id], Tags=[{'Key': k, 'Value': v}])
                 logging.info("Successfully added tags for EC2 {} instance".format(instance_id))
             print("Successfully added tags for EC2 {} instance".format(instance_id))
-        print("Completed!!")
+
+        print("Tagging is Completed for EC {} Instance!!".format(instance_id))
--- aws-resource-tagging/ec2_autotag.py
+++ aws-resource-tagging/ec2_autotag.py
@@ -1,21 +1,29 @@
-from autotags import AutoTagAWSResources
+#!/usr/bin/env python3
+
 import argparse
+from autotags import AutoTagAWSResources
+
+
+def main(key, secret, region):
+    ec2conn = AutoTagAWSResources(key=key, secret=secret)
+    instances = ec2conn.get_instances(region=region)
+    for instance_id, region in instances.items():
+        owner = ec2conn.get_resource_owner(instance_id, region)
+        ec2conn.update_ec2_tags(instance_id, region, Owner="{}".format(owner), OwnerEmail="{}@redhat.com".format(owner), Project="None", CostCode="None", Purpose="None", Role="None", Environment="None")
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='PROG', usage='%(prog)s [options]', description='Python Boto3 AWS Framework')
     parser.add_argument('--key', action='store', dest='key', help='AWS Key')
     parser.add_argument('--secret', action='store', dest='secret', help='AWS Secret')
+    parser.add_argument('--region', action='store', dest='region', default='us-east-1', help='AWS Region')
+
     args = parser.parse_args()
 
     if args.key is None:
-        raise ValueError("Error: AWS Key is missing!")
+        raise ValueError("Error: AWS Key cannot be Null!")
     elif args.secret is None:
-        raise ValueError("Error: AWS Secret is missing!")
-
-    ec2conn = AutoTagAWSResources(key=args.key, secret=args.secret)
-    instances = ec2conn.get_all_instances()
-    for instance_id, region in instances.items():
-        print(instance_id, region)
-        owner = ec2conn.get_resource_owner(instance_id, region)
-        print(owner)
+        raise ValueError("Error: AWS Secret cannot be Null!")
+    elif args.region is None:
+        raise ValueError("Error: AWS Region cannot be Null!")
+    main(args.key, args.secret, args.region)
